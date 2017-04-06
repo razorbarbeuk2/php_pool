3adc23e946e929ecb467d7a67ca3a09ef319240d
gbourson <gbourson@student42.fr>
2017-04-06 13:14:15 +0200

    J02

--- j02/ex01/one_more_time.php
+++ j02/ex01/one_more_time.php
@@ -12,16 +12,46 @@ if ($argc > 1)
 
 function check_date($val)
 {
-	$day = array("Lundi");
-	if(preg_match('/(Lundi?|Mardi?|Mercredi?|Jeudi?|Vendredi?|Samedi?|Dimanche?)\s\d{1,2}\s(Janvier?|Février?|Mars?|Avril?|Mai?|Juin?|Juillet?|Aout?|Septembre?|Octobre?|Novembre?|Decembre?)\s\d{4}\s\d{2}:\d{2}:\d{2}/', $val))
+	$day = array
+	(
+		array("lundi", "[L,l]undi"),
+		array("mardi", "[M,m]ardi"),
+		array("mercredi", "[M,m]ercredi"),
+		array("jeudi", "[J,j]eudi"),
+		array("vendredi", "[V,v]endredi"),
+		array("samedi", "[S,s]amedi"),
+		array("dimanche", "[D,d]imache"),
+		);
+
+	$month = array
+	(
+		array("janvier", "[J,j]anvier"),
+		array("fevrier", "[F,f][e,é]vrier"),
+		array("mars", "[M,m]ars"),
+		array("avril", "[A,a]vril"),
+		array("mai", "[M,m]ai"),
+		array("juin", "[J,j]uin"),
+		array("juillet", "[J,j]uillet"),
+		array("aout", "[A,a]o[u,û]t"),
+		array("septembre", "[S,s]eptembre"),
+		array("octobre", "[O,o]ctobre"),
+		array("novembre", "[N,n]ovembre"),
+		array("decembre", "[D,d][e,é]cembre"),
+	);
+
+	if(preg_match('/('.$day[0][1].'?|'.$day[1][1].'?|'.$day[2][1].'?|'.$day[3][1].'?|'.$day[4][1].'?|'.$day[5][1].'?|'.$day[6][1].'?)\s\d{1,2}\s('.$month[0][1].'?|'.$month[1][1].'?|'.$month[2][1].'?|'.$month[3][1].'?|'.$month[4][1].'?|'.$month[5][1].'?|'.$month[6][1].'?|'.$month[7][1].'?|'.$month[8][1].'?|'.$month[9][1].'?|'.$month[10][1].'?|'.$month[11][1].'?)\s\d{4}\s\d{2}:\d{2}:\d{2}/', $val))
 	{
 		$tab = explode(' ', $val);
-		foreach ($tab as $key => $value) 
+		date_default_timezone_set('Europe/Paris');
+		foreach ($month as $key => $value) 
 		{
-			echo $value;
+			if (preg_match('/'.$value[1].'/', $tab[2]))
+				$len_month = $key + 1;
 		}
-		//$str_f = $tab[1]."/".$tab[3]."/".$tab[5];
-		
+		$t = strtotime($tab[3]."/".$len_month."/".$tab[1]." ".$tab[4]);
+		echo $t;
 	}
+	else
+		echo "Wrong Format\n";
 }
 ?>
